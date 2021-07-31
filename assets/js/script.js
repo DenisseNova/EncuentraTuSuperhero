0ed18540d65b917a491b628dba8477d6f01ce1b0
Denisse Nova <denisse.nova.v@gmail.com>
2021-07-31 17:48:33 -0400

    info card js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,51 @@ $(document).ready(function(){
     e.preventDefault();
     const id = $('#inputIdSH').val();
     const miUrl = `https://www.superheroapi.com/api.php/${miToken}/${id}`
+    $.ajax({
+      url: miUrl,
+      success: function(respuesta){
+        if (respuesta.response === 'error') {
+          alert('No existe el ID');
+          console.log(respuesta)
+        } else  if (respuesta.response === 'success') {
+          console.log(respuesta)
+          console.log(respuesta.powerstats.combat)
+          $("#info").html(
+            `<div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img src="${respuesta.image.url}">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">${respuesta.name} </h5>
+                  <p class="card-text">Conexiones: ${respuesta.connections['group-affiliation']}</p>
+                  <p class="card-text">Publicado por: ${respuesta.biography.publisher}</p>
+                  <hr>
+                  <p class="card-text">Ocupación: ${respuesta.work.occupation}</p>
+                  <hr>
+                  <p class="card-text">Primera Aparición: ${respuesta.biography["first-appearance"]}</p>
+                  <hr>
+                  <p class="card-text">Altura: ${respuesta.appearance.height[0]} - ${respuesta.appearance.height[1]}</p>
+                  <hr>
+                  <p class="card-text">Peso: ${respuesta.appearance.weight[0]} - ${respuesta.appearance.weight[1]}</p>
+                  <hr>
+                  <p class="card-text">Alianzas: ${respuesta.biography.aliases}</p>
+                </div>
+              </div>
+            </div>
+          </div>`
+          )
+        }
+      },
+      error: function(){
+        console.log("no se ha podido obtener la info");
+      }
+    });
   })
 })
+
+
 
 var options = {
 	title: {
@@ -32,6 +75,6 @@ var options = {
 		]
 	}]
 };
-
-
 $("#chartContainer").CanvasJSChart(options);   
+
+$(".esconder").show();
