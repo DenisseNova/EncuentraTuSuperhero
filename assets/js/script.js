48ea7edf3f86d4a1bc5308a730d85803192d1007
Denisse Nova <denisse.nova.v@gmail.com>
2021-08-08 19:19:58 -0400

    validacion de numero js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,15 @@ $(document).ready(function(){
     $(".esconder").hide();
 
     const id = $('#inputIdSH').val();
+    if(isNaN(id)) return alert('Debe ingesar un numero')
+    
     const miUrl = `https://www.superheroapi.com/api.php/${miToken}/${id}`
     $.ajax({
       url: miUrl,
       success: function(respuesta){
         if (respuesta.response === 'error') {
           alert('No existe el ID');
-          console.log(respuesta)
         } else  if (respuesta.response === 'success') {
-          console.log(respuesta)
-          console.log(respuesta.powerstats.combat)
           $("#info").html(
             `<div class="card mb-3">
             <div class="row no-gutters">
@@ -24,7 +23,7 @@ $(document).ready(function(){
               <div class="col-md-8">
                 <div class="card-body">
                   <h5 class="card-title">${respuesta.name} </h5>
-                  <p class="card-text">Conexiones: ${respuesta.connections['group-affiliation']}</p>
+                  <p class="card-text"><small>Conexiones: ${respuesta.connections['group-affiliation']}</small></p>
                   <p class="card-text">Publicado por: ${respuesta.biography.publisher}</p>
                   <hr>
                   <p class="card-text">Ocupación: ${respuesta.work.occupation}</p>
@@ -56,9 +55,8 @@ $(document).ready(function(){
         }
       },
       error: function(){
-        console.log("no se ha podido obtener la info");
         $(".esconder").hide();
-      }
+      },
     });
   })
 })
